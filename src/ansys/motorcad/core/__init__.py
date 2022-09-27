d618778ca928ff89730a61d76636a1a46784573f
jgsdavies <80680946+jgsdavies@users.noreply.github.com>
2022-09-27 14:18:53 +0100

    Add MotorCAD_Methods.py with requirements

--- src/ansys/motorcad/core/__init__.py
+++ src/ansys/motorcad/core/__init__.py
@@ -10,3 +10,5 @@ except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
+
+from ansys.motorcad.core.MotorCAD_Methods import *
