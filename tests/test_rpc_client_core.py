15ddbf5e969bb8ce44a8e67fc77a4a3c459f851d
Jack Davies <jack.davies@ansys.com>
2022-12-12 14:14:10 +0000

    revert changes to rpc_client_core.py

---
+++ tests/test_rpc_client_core.py
@@ -0,0 +1,10 @@
+from ansys.motorcad.core import MotorCAD
+from setup_test import setup_test_env
+
+# Get Motor-CAD exe
+mc = setup_test_env()
+
+
+def test__find_free_motor_cad():
+    # Test if we can find open Motor-CAD instance
+    mc2 = MotorCAD(open_new_instance=False)
