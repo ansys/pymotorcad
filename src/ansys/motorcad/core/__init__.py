d8e9504c447cbf91d8a360f86832d49ad17f74ac
MaxJPRey <maxime.rey@ansys.com>
2023-01-13 15:52:38 +0100

    Get the version dynamically.

--- src/ansys/motorcad/core/__init__.py
+++ src/ansys/motorcad/core/__init__.py
@@ -1,11 +1,6 @@
 """motorcad.core."""
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+__version__ = "0.1.dev0"
 
 from ansys.motorcad.core.MotorCAD_Methods import MotorCAD, MotorCADCompatibility
 from ansys.motorcad.core.rpc_client_core import (
