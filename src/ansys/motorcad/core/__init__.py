6d7475c1d8644cb084947c5be633555e50fc8e38
ravi-ansys <ravi.sundaria@ansys.com>
2023-01-16 15:55:03 +0000

    imported geometry.py in __init__.py

--- src/ansys/motorcad/core/__init__.py
+++ src/ansys/motorcad/core/__init__.py
@@ -8,6 +8,7 @@ except ModuleNotFoundError:
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
 from ansys.motorcad.core.MotorCAD_Methods import MotorCAD, MotorCADCompatibility
+import ansys.motorcad.core.geometry
 from ansys.motorcad.core.rpc_client_core import (
     MotorCADError,
     set_default_instance,
