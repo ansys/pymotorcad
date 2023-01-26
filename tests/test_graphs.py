7b2b1897d4fee3dd88d4da55d38a9239e619eb79
ravi-ansys <ravi.sundaria@ansys.com>
2023-01-26 12:11:47 +0000

    testing barch modification which already had a pull request

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,5 +1,7 @@
 from RPC_Test_Common import almost_equal
 from setup_test import reset_to_default_file, setup_test_env
+import pytest
+from ansys.motorcad.core import MotorCADError
 
 # Get Motor-CAD exe
 mc = setup_test_env()
@@ -15,3 +17,21 @@ def test_get_magnetic_graph_point():
     x, y = mc.get_magnetic_graph_point("TorqueVW", 3)
     assert almost_equal(x, 360)
     assert almost_equal(y, 180.14)
+
+
+def test_get_magnetic_graph():
+    reset_to_default_file(mc)
+    mc.set_variable("TorqueCalculation", True)
+
+    mc.do_magnetic_calculation()
+
+    x, y = mc.get_magnetic_graph_point("TorqueVW", 3)
+    x1,y1 = mc.get_magnetic_graph("TorqueVW")
+
+    assert almost_equal(x, x1[3])
+    assert almost_equal(y, y1[3])
+
+    with pytest.raises(MotorCADError):
+        x, y = mc.get_magnetic_graph("ediujhweioufbewkijbf")
+# Get Motor-CAD exe
+
