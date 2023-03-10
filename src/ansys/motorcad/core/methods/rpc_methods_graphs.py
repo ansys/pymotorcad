531d8ebb149f4ee2344df93b9cd03e0df6b61bb6
JackDavies <jack.davies@ansys.com>
2023-03-10 12:38:42 +0000

    Minor changes/fixes

--- src/ansys/motorcad/core/methods/rpc_methods_graphs.py
+++ src/ansys/motorcad/core/methods/rpc_methods_graphs.py
@@ -2,6 +2,46 @@
 from ansys.motorcad.core.rpc_client_core import MotorCADError
 
 
+def _get_graph(graphing_func, *args):
+    """Get an array from a Motor-CAD graph specific to input graph function name.
+
+    Parameters
+    ----------
+    graphing_func : func
+        graph function to use eg. get_magnetic_graph_point
+    *args : str, int
+        Name/id of graph to select. Graph name is preferred and can be found in
+        Motor-CAD (help -> graph viewer) for eg. TorqueVW
+
+    Returns
+    -------
+    x_array : array
+        Values of x coordinates from the graph.
+    y_array : array
+        Values of y coordinates from the graph.
+    """
+    loop = 0
+    x_array = []
+    y_array = []
+    while True:
+        try:
+            x, y = graphing_func(*args, loop)
+
+            y_array.append(y)
+            x_array.append(x)
+            loop += 1
+
+        except MotorCADError as e:
+            break
+            # Can't be used currently: Bug in Motor-CAD RPC server
+            # if "Point requested is greater than number of points available" in str(e):
+            #     break
+            # else:
+            #     raise
+
+    return x_array, y_array
+
+
 class _RpcMethodsGraphs:
     def __init__(self, mc_connection):
         self.connection = mc_connection
@@ -28,101 +68,62 @@ class _RpcMethodsGraphs:
         params = [{"variant": graph_name}, point_number]
         return self.connection.send_and_receive(method, params)
 
-    def _get_graph(self, graphing_func, *args):
-        """Get an array from a Motor-CAD graph specific to input graph function name.
-
-        Parameters
-        ----------
-        graphing_func : str
-            Name of graph function for eg. get_magnetic_graph_point
-        *args : str, int
-            Name/id of graph to select. Graph name is preferred and can be found in
-            Motor-CAD (help -> graph viewer) for eg. TorqueVW
-
-        Returns
-        -------
-        x_array : array
-            Values of x coordinates from the graph.
-        y_array : array
-            Values of y coordinates from the graph.
-        """
-        loop = 0
-        x_array = []
-        y_array = []
-        while True:
-            try:
-                x, y = graphing_func(*args, loop)
-
-                y_array.append(y)
-                x_array.append(x)
-                loop = loop + 1
-
-            except MotorCADError as e:
-                break
-                # Can't be used currently: Bug in Motorcad RPC server
-                # if "Point requested is greater than number of points available" in str(e):
-                #     break
-                # else:
-                #     raise
-
-        return x_array, y_array
-
     def get_magnetic_graph(self, graph_name):
-        """Get array from a Motor-CAD Magnetic graph.
+        """Get graph points from a Motor-CAD Magnetic graph.
 
         Parameters
         ----------
-        graph_name : str|int
+        graph_name : str, int
             Name/id of graph to select. Graph name is preferred and can be found in
             Motor-CAD (help -> graph viewer)
         Returns
         -------
-        x_array : array
+        x_array : list
             value of x coordinates from graph
-        y_array : array
+        y_array : list
             value of y coordinates from graph
         """
         loop = 0
         x_array = []
         y_array = []
-        return self._get_graph(self.get_magnetic_graph_point, graph_name)
+        return _get_graph(self.get_magnetic_graph_point, graph_name)
 
     def get_temperature_graph(self, graph_name):
-        """Get array from a Motor-CAD transient temperature  graph from thermal module of Motor-CAD.
+        """Get graph points from a Motor-CAD transient temperature graph.
 
         Parameters
         ----------
-        graph_name : str|int
+        graph_name : str, int
             Name/id of graph to select. Graph name is preferred and can be found in
             Motor-CAD (help -> graph viewer)
         Returns
         -------
-        x_array : array
+        x_array : list
             value of x coordinates from graph
-        y_array : array
+        y_array : list
             value of y coordinates from graph
         """
         loop = 0
         x_array = []
         y_array = []
-        return self._get_graph(self.get_temperature_graph_point, graph_name)
+        return _get_graph(self.get_temperature_graph_point, graph_name)
 
     def get_power_graph(self, graph_name):
-        """Get array from a Motor-CAD transient power loss graph from thermal module of Motor-CAD.
+        """Get graph points from a Motor-CAD transient power loss graph.
 
         Parameters
         ----------
-        graph_name : str|int
+        graph_name : str, int
             Name/id of graph to select. Graph name is preferred and can be found in
             Motor-CAD (help -> graph viewer)
         Returns
         -------
-        x_array : array
+        x_array : list
             value of x coordinates from graph
-        y_array : array
+        y_array : list
             value of y coordinates from graph
         """
-        return self._get_graph(self.get_power_graph_point, graph_name)
+        return _get_graph(self.get_power_graph_point, graph_name)
 
     def get_temperature_graph_point(self, graph_name, point_number):
         """Get a point from a Motor-CAD thermal graph.
