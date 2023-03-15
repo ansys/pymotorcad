3900e2bb0136708a374b1c603d8f634b97b9f991
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-03-15 16:56:03 +0100

    doc: try to add version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -44,7 +44,7 @@ html_theme_options = {
     ],
     "switcher": {
         "json_url": f"https://{cname}/versions.json",
-        "version_match": get_version_match(version),
+        "version_match": get_version_match(__version__),
     },
 }
 
