b6d6e6e69fd8c2a0e76925e317b8e28527fadf2b
Jack Davies <jack.davies@ansys.com>
2023-11-14 09:50:03 +0000

    wrongly categorised methods

--- src/ansys/motorcad/core/methods/rpc_methods_fea_geometry.py
+++ src/ansys/motorcad/core/methods/rpc_methods_fea_geometry.py
@@ -8,36 +8,6 @@ class _RpcMethodsFEAGeometry:
     def __init__(self, mc_connection):
         self.connection = mc_connection
 
-    def set_power_injection_value(self, name, node1, value, rpm_ref, rpm_coef, description):
-        """Set or create a power injection.
-
-        Parameters
-        ----------
-        name : str
-            Name of the power injection.
-        node1 :
-        value :
-        rpm_ref :
-        rpm_coef :
-        description : str
-
-        """
-        method = "SetPowerInjectionValue"
-        params = [name, node1, value, rpm_ref, rpm_coef, description]
-        return self.connection.send_and_receive(method, params)
-
-    def set_fixed_temperature_value(self, name, node1, value, description):
-        """Set or create a fixed temperature on a node."""
-        method = "SetFixedTemperatureValue"
-        params = [name, node1, value, description]
-        return self.connection.send_and_receive(method, params)
-
-    def clear_fixed_temperature_value(self, node1):
-        """Remove a fixed temperature from a node."""
-        method = "ClearFixedTemperatureValue"
-        params = [node1]
-        return self.connection.send_and_receive(method, params)
-
     def do_slot_finite_element(self):
         """Run slot FEA."""
         method = "DoSlotFiniteElement"
