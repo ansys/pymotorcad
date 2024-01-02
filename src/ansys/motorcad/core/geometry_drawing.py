8fbc3bec49544b087d4176017c88994d5ce6c703
Jack Davies <jack.davies@ansys.com>
2024-01-02 09:10:49 +0000

    Update geometry_drawing to allow drawing of Coordinates and Entities

--- src/ansys/motorcad/core/geometry_drawing.py
+++ src/ansys/motorcad/core/geometry_drawing.py
@@ -1,7 +1,8 @@
 """Unit containing region drawing functions."""
 from copy import deepcopy
+from warnings import warn
 
-from ansys.motorcad.core.geometry import Arc, Coordinate, Line
+from ansys.motorcad.core.geometry import Arc, Coordinate, Entity, Line, Region
 
 try:
     import matplotlib.patches as mpatches
@@ -60,41 +61,8 @@ class _RegionDrawing:
 
     def draw_region(self, region, colour):
         """Draw a region."""
-        entity_coords = []
-
         for entity in region.entities:
-            mid_point = Coordinate(
-                (entity.end.x + entity.start.x) / 2, (entity.end.y + entity.start.y) / 2
-            )
-
-            entity_coords += [Coordinate(mid_point.x, mid_point.y)]
-
-            if isinstance(entity, Line):
-                plt.plot(
-                    [entity.start.x, entity.end.x], [entity.start.y, entity.end.y], color=colour
-                )
-
-            elif isinstance(entity, Arc):
-                width = abs(entity.radius * 2)
-                height = abs(entity.radius * 2)
-                centre = entity.centre.x, entity.centre.y
-                rad1, angle1 = (entity.start - entity.centre).get_polar_coords_deg()
-                rad2, angle2 = (entity.end - entity.centre).get_polar_coords_deg()
-
-                if entity.radius > 0:
-                    start_angle = angle1
-                    end_angle = angle2
-                else:
-                    start_angle = angle2
-                    end_angle = angle1
-
-                arc = mpatches.Arc(
-                    centre, width, height, theta1=start_angle, theta2=end_angle, color=colour
-                )
-                self.ax.plot(marker="-o")
-                self.ax.add_patch(arc)
-
-        self.ax.set_aspect("equal", adjustable="box")
+            self.draw_entity(entity, colour)
 
         for entity_num, entity in enumerate(region.entities):
             text = "e{}".format(entity_num)
@@ -107,39 +75,94 @@ class _RegionDrawing:
 
         self._plot_text_no_overlap(region.centroid, region.name, colour)
 
+    def draw_coordinate(self, coordinate, colour):
+        """Draw coordinate onto plot."""
+        plt.plot(coordinate.x, coordinate.y, "x", color=colour)
 
-def draw_regions(regions):
-    """Draw regions on plot.
+    def draw_entity(self, entity, colour):
+        """Draw entity onto plot."""
+        entity_coords = []
 
-    Parameters
-    ----------
-    regions : Region or list of Region
-        entities to draw
-    """
+        mid_point = Coordinate(
+            (entity.end.x + entity.start.x) / 2, (entity.end.y + entity.start.y) / 2
+        )
+
+        entity_coords += [Coordinate(mid_point.x, mid_point.y)]
+
+        if isinstance(entity, Line):
+            plt.plot([entity.start.x, entity.end.x], [entity.start.y, entity.end.y], color=colour)
+
+        elif isinstance(entity, Arc):
+            width = abs(entity.radius * 2)
+            height = abs(entity.radius * 2)
+            centre = entity.centre.x, entity.centre.y
+            rad1, angle1 = (entity.start - entity.centre).get_polar_coords_deg()
+            rad2, angle2 = (entity.end - entity.centre).get_polar_coords_deg()
+
+            if entity.radius > 0:
+                start_angle = angle1
+                end_angle = angle2
+            else:
+                start_angle = angle2
+                end_angle = angle1
+
+            arc = mpatches.Arc(
+                centre, width, height, theta1=start_angle, theta2=end_angle, color=colour
+            )
+            self.ax.plot(marker="-o")
+            self.ax.add_patch(arc)
+
+        self.ax.set_aspect("equal", adjustable="box")
+
+
+def draw_objects(objects):
+    """Draw geometry objects on a plot."""
     if not MATPLOTLIB_AVAILABLE:
         raise ImportError(
             "Failed to draw geometry. Please ensure MatPlotLib and a suitable backend "
             "e.g. PyQt5 are installed"
         )
 
-    if not isinstance(regions, list):
+    if not isinstance(objects, list):
         # Given a single region not a list - reformat
-        regions = [regions]
+        objects = [objects]
 
     stored_coords = []
 
     # Some basic colours
     colours = ["red", "blue", "green", "purple"]
-
+    entity_no_region_colour = "grey"
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
     region_drawing = _RegionDrawing(ax, stored_coords)
-    for i, region in enumerate(regions):
+    for i, object in enumerate(objects):
         # loop through colours once end of list reached
         while i > len(colours) - 1:
             i -= len(colours)
-
-        region_drawing.draw_region(region, colours[i])
-
+        if isinstance(object, Region):
+            region_drawing.draw_region(object, colours[i])
+        elif isinstance(object, Entity):
+            region_drawing.draw_entity(object, entity_no_region_colour)
+        elif isinstance(object, Coordinate):
+            region_drawing.draw_coordinate(object, entity_no_region_colour)
+        else:
+            raise TypeError("Object cannot be drawn")
     plt.show()
+
+
+def draw_regions(regions):
+    """WILL BE DEPRECATED SOON - USE geometry_drawing.draw_objects() instead.
+
+    Draw regions on plot.
+
+    Parameters
+    ----------
+    regions : Region or list of Region
+        entities to draw
+    """
+    warn(
+        "draw_regions() WILL BE DEPRECATED SOON - USE geometry_drawing.draw_objects instead",
+        DeprecationWarning,
+    )
+    draw_objects(regions)
