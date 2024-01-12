6c228347e1202a589536015f3b6fac72660a2aa2
ravi-ansys <ravi.sundaria@ansys.com>
2024-01-12 16:11:58 +0000

    move to rpc_methods_geometry

--- src/ansys/motorcad/core/methods/rpc_methods_geometry.py
+++ src/ansys/motorcad/core/methods/rpc_methods_geometry.py
@@ -1,5 +1,5 @@
 """RPC methods for geometry."""
-
+import os
 
 class _RpcMethodsGeometry:
     def __init__(self, mc_connection):
@@ -86,3 +86,56 @@ class _RpcMethodsGeometry:
         method = "CheckIfGeometryIsValid"
         params = [edit_geometry]
         return self.connection.send_and_receive(method, params)
+    
+
+    def template_list(self):
+        """Return List of templates available in Motor-CAD.
+
+            Individual template information is in dictionary format
+            for example
+            [{'Template': 'a1',
+            'Sector': 'Aerospace',
+        '     Machine_Type': ' Surface PM',
+            'Application': ' Generator',
+            'Winding_Type': ' Concentrated',
+            'Max_Speed(rpm)': ' 10,000',
+            'Power(kW)': ' 14',
+        '     Cooling': 'Oil cooled',
+        '     Drive_Type': 'Sine'},]
+        """
+    
+
+        # Motor-CAD installation directory in PC
+        mcad_install_dir = self.get_variable("CurrentExeDir_MotorLAB")
+        directory = mcad_install_dir + r"Motor-CAD Data\templates"
+        template_list1 = []
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            if filename.endswith(".mtt"):
+                with open(f, "r") as file:
+                    print(filename)
+                    for l_no, line in enumerate(file):
+                        # search string
+                        if "Data_File_Notes[1]" in line:
+                            print("string found in a file")
+                            print("Line Number:", l_no)
+                            print("Line:", line)
+                            # don't look for next lines
+                            list1 = line.split(";")
+                            list1[0] = list1[0][19:]
+                            dict1 = {
+                                "Template": filename.split(".")[0],
+                                "Sector": list1[0],
+                                "Machine_Type": list1[1],
+                                "Application": list1[2],
+                                "Winding_Type": list1[3],
+                                "Max_Speed(rpm)": list1[4],
+                                "Power(kW)": list1[5],
+                                "Cooling": list1[6],
+                                "Drive_Type": list1[7],
+                            }
+                            template_list1.append(dict1)
+                            break
+
+        return template_list1
+
