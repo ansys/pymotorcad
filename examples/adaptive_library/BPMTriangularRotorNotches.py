d41ab05815f5b4850e9ab8bf8d2c3fcb41746efc
JackB-Ansys <jack.beane@ansys.com>
2024-02-06 16:45:59 +0000

    Updated BPMTriangularRotorNotches.py to use _is_running_in_internal_scripting() variable

--- examples/adaptive_library/BPMTriangularRotorNotches.py
+++ examples/adaptive_library/BPMTriangularRotorNotches.py
@@ -7,87 +7,68 @@ Adaptive Template script to create triangular rotor notches to improve NVH perfo
 """
 # %%
 # .. note::
-#    For more information on the use of Adaptive Templates in Motor-CAD,
-#    and how to create, modify and debug Adaptive Templates Scripts,
-#    see :ref:`ref_adaptive_templates_UG` in the :ref:`ref_user_guide`.
+#    For more information on the use of Adaptive Templates in Motor-CAD, and how to create, modify
+#    and debug Adaptive Templates Scripts, see :ref:`ref_adaptive_templates_UG` in the
+#    :ref:`ref_user_guide`.
 #
-# This script is designed to be run from Motor-CAD template "e9".
-# If no Motor-CAD file is open,
-# the e9 template will be loaded.
+# This script is designed to be run from Motor-CAD template "e9". If no Motor-CAD file is open, the
+# e9 template will be loaded.
 #
 # This script uses the following adaptive parameters:
 #
 # * Notch Angle (-4)
 #
-# * Notch Sweep (2)
+# * Notch Sweep (5)
 #
 # * Notch Depth (1)
 #
 # * Notches per Pole (2)
 #
-# If these parameters are not already set up in the Motor-CAD file,
-# the parameters will be automatically set,
-# with the default values shown in brackets.
+# If these parameters are not already set up in the Motor-CAD file, the parameters will be
+# automatically set, with the default values shown in brackets.
 #
-# To set an adaptive geometry for a Motor-CAD file,
-# a script must be loaded in to the Adaptive Templates tab
-# (Geometry -> Editor -> Adaptive Templates) in Motor-CAD
-# and run.
-# When the option 'Geometry Templates Type' is set to 'Adaptive',
-# this script is automatically run repeatedly
-# to keep the Adaptive Geometry set in Motor-CAD.
+# To set an adaptive geometry for a Motor-CAD file, a script must be loaded in to the Adaptive
+# Templates tab (Geometry -> Editor -> Adaptive Templates) in Motor-CAD and run. When the option
+# 'Geometry Templates Type' is set to 'Adaptive', this script is automatically run repeatedly to
+# keep the Adaptive Geometry set in Motor-CAD.
 #
 # .. image:: ../../images/Adaptive_Templates_GUI_Screenshot.png
 #
-# This Python script can also be executed externally,
-# but unless the script is loaded in to the Adaptive Templates tab
-# in Motor-CAD, the geometry will only be defined temporarily.
+# This Python script can also be executed externally. When executed externally, a Motor-CAD instance
+# will be launched and a file based on the "e9" template will be saved to a temporary folder. This
+# script will be loaded into the Adaptive Templates tab.
 
 # %%
 # Perform Required imports
 # ------------------------
-# Import pymotorcad to access Motor-CAD.
-# Import triangular_notch to create the notch geometry region
-# with Adaptive Template geometry.
-# Import os, tempfile and shutil
-# to open and save a temporary .mot file if none is open.
+# Import pymotorcad to access Motor-CAD. Import triangular_notch to create the notch geometry region
+# with Adaptive Template geometry. Import os, tempfile and shutil to open and save a temporary .mot
+# file if none is open.
 import os
 import shutil
+import sys
 import tempfile
 
 import ansys.motorcad.core as pymotorcad
 from ansys.motorcad.core.geometry_shapes import triangular_notch
+from ansys.motorcad.core.rpc_client_core import _is_running_in_internal_scripting
 
 # %%
 # Connect to Motor-CAD
 # --------------------
-# If this script is loaded into the Adaptive Templates file in Motor-CAD,
-# the current Motor-CAD instance will be used.
+# If this script is loaded into the Adaptive Templates file in Motor-CAD, the current Motor-CAD
+# instance will be used.
 #
-# If the script is run externally,
-# and a Motor-CAD instance is currently open,
-# that Motor-CAD instance will be used.
-# If no Motor-CAD instance is open,
-# a new Motor-CAD instance will be opened.
-# To keep a new Motor-CAD instance open after executing the script,
-# use the option ``mc = pymotorcad.MotorCAD(reuse_parallel_instances=True)``
-# when opening the new instance.
-try:
+# If the script is run externally: a new Motor-CAD instance will be opened, the e9 IPM motor
+# template will be loaded and the file will be saved to a temporary folder.
+# To keep a new Motor-CAD instance open after executing the script, use the option
+# ``mc = pymotorcad.MotorCAD(reuse_parallel_instances=True)`` when opening the new instance.
+
+if _is_running_in_internal_scripting():
     # Use existing Motor-CAD instance if possible
     mc = pymotorcad.MotorCAD(open_new_instance=False)
-    mc.connection.ensure_version_at_least("2024.1.1")
-except pymotorcad.MotorCADError:
-    # Otherwise open a new instance
+else:
     mc = pymotorcad.MotorCAD()
-
-# %%
-# Load file if required
-# ---------------------
-# Check if a file is loaded already.
-# If not, open the e9 IPM motor template,
-# save the file to a temporary folder.
-if not mc.get_variable("CurrentMotFilePath_MotorLAB"):
-    mode = "External_Run"
     # Disable popup messages
     mc.set_variable("MessageDisplayState", 2)
     mc.set_visible(True)
@@ -103,25 +84,20 @@ if not mc.get_variable("CurrentMotFilePath_MotorLAB"):
     mot_name = "BPMTriRotorNotches"
     mc.save_to_file(working_folder + "/" + mot_name + ".mot")
 
-else:
-    mode = "Internal_Run"
+# Reset geometry to default
+mc.reset_adaptive_geometry()
 
 # %%
 # Set Adaptive Parameters if required
 # -----------------------------------
-# Four Adaptive Parameters are required
-# for this adaptive template.
-# These are used to define the
-# number of rotor notches to be added,
-# their position and size.
+# Four Adaptive Parameters are required for this adaptive template. These are used to define the
+# number of rotor notches to be added, their position and size.
 #
-# If the Adaptive Parameters have already been set in the current Motor-CAD file,
-# their current values will be used.
-# Otherwise, the Adaptive Parameters will be defined
-# and set to default values.
+# If the Adaptive Parameters have already been set in the current Motor-CAD file, their current
+# values will be used. Otherwise, the Adaptive Parameters will be defined and set to default values.
 #
-# The function ``set_default_parameter`` is defined to check if a parameter exists,
-# and if not, create it with a default value.
+# The function ``set_default_parameter`` is defined to check if a parameter exists, and if not,
+# create it with a default value.
 
 
 def set_default_parameter(parameter_name, default_value):
@@ -134,7 +110,7 @@ def set_default_parameter(parameter_name, default_value):
 # %%
 # Use the ``set_default_parameter`` to set the required parameters if undefined
 set_default_parameter("Notch Angle", -4)
-set_default_parameter("Notch Sweep", 2)
+set_default_parameter("Notch Sweep", 5)
 set_default_parameter("Notch Depth", 1)
 set_default_parameter("Notches per Pole", 2)
 
@@ -149,19 +125,16 @@ notch_depth = mc.get_adaptive_parameter_value("notch depth")
 number_notches = int(mc.get_adaptive_parameter_value("notches per pole"))
 
 # %%
-# Get the standard template rotor region from Motor-CAD.
-# Calculate the rotor radius
-# and define the rotor centre coordinates.
+# Get the standard template rotor region from Motor-CAD. Calculate the rotor radius and define the
+# rotor centre coordinates.
 rotor_region = mc.get_region("Rotor")
 
 rotor_radius = mc.get_variable("RotorDiameter") / 2
 duplication_angle = 360 / rotor_region.duplications
 
 # %%
-# Add an ``if`` statement to account for the case
-# when a notch crosses the symmetry boundary.
-# This resets ``notch_angle`` to half the ``notch_angular_width``
-# away from the boundary.
+# Add an ``if`` statement to account for the case when a notch crosses the symmetry boundary. This
+# resets ``notch_angle`` to half the ``notch_angular_width`` away from the boundary.
 if notch_angle > (duplication_angle / (2 * number_notches)) - (notch_angular_width / 2):
     # Limit so notch does not cross the symmetry boundary
     notch_angle = (duplication_angle / (2 * number_notches)) - notch_angular_width / 2
@@ -169,15 +142,14 @@ if notch_angle > (duplication_angle / (2 * number_notches)) - (notch_angular_wid
     mc.set_adaptive_parameter_value("notch angle", notch_angle)
 
 # %%
-# Add an ``if`` statement to account for the case
-# when notches overlap at the centre of the pole.
+# Add an ``if`` statement to account for the case when notches overlap at the centre of the pole.
 # This resets ``notch_angle`` to:
 #
-# * Half the ``notch_angular_width``
-#   away from the pole centre when there are an even number of notches
+# * Half the ``notch_angular_width`` away from the pole centre when there are an even number of
+#   notches
 #
-# * The full ``notch_angular_width``
-#   away from the pole centre when there are an odd number of notches
+# * The full ``notch_angular_width`` away from the pole centre when there are an odd number of
+#   notches
 if number_notches % 2 == 0:
     x = -1
 else:
@@ -194,17 +166,14 @@ if notch_angle < -((duplication_angle / (2 * number_notches)) - (notch_angular_w
 # --------------------------------------
 # For each notch to be added:
 #
-# * Calculate the angular position of the notch
-#   in mechanical degrees
+# * Calculate the angular position of the notch in mechanical degrees
 #
-# * Apply the offset angle. For notches on the left side of the pole,
-#   the position is shifted by ``+ notch_angle`` mechanical degrees.
-#   For notches on the right side of the pole,
-#   the position is shifted by ``- notch_angle`` mechanical degrees.
+# * Apply the offset angle. For notches on the left side of the pole, the position is shifted by
+#   ``+ notch_angle`` mechanical degrees. For notches on the right side of the pole, the position is
+#   shifted by ``- notch_angle`` mechanical degrees.
 #
-# * Create the notch Region using the ``triangular_notch()``
-#   function, imported from ``ansys.motorcad.core.geometry_shapes``.
-#   The arguments for the function are:
+# * Create the notch Region using the ``triangular_notch()`` function, imported from
+#   ``ansys.motorcad.core.geometry_shapes``. The arguments for the function are:
 #
 #   * rotor_radius
 #
@@ -224,9 +193,8 @@ if notch_angle < -((duplication_angle / (2 * number_notches)) - (notch_angular_w
 #
 #   * material
 #
-# * set the notch's ``parent`` to the rotor region.
-#   This will allow Motor-CAD to treat the notch as a sub-region
-#   of the rotor and handle subtractions automatically.
+# * set the notch's ``parent`` to the rotor region. This will allow Motor-CAD to treat the notch as
+#   a sub-region of the rotor and handle subtractions automatically.
 #
 # * If the notch is closed, set the region in Motor-CAD.
 
@@ -256,10 +224,23 @@ for notch_loop in range(0, number_notches):
         mc.set_region(notch)
 
 # %%
-# .. image:: ../../images/BPMTriangularRotorNotches.png
+# Load in Adaptive Templates Script if required
+# ---------------------------------------------
+# When the script is run externally:
+#
+# * Set Geometry type to "Adaptive"
+#
+# * Load the script into the Adaptive Templates tab
+#
+# * Go to the Geometry -> Radial tab to run the Adaptive Templates Script and display the new
+#   geometry
 
 
-if mode == "External_Run":
+if not _is_running_in_internal_scripting():
     mc.set_variable("GeometryTemplateType", 1)
-    mc.load_adaptive_script(__file__)
+    mc.load_adaptive_script(sys.argv[0])
     mc.display_screen("Geometry;Radial")
+
+# %%
+# .. image:: ../../images/BPMTriangularRotorNotches.png
+#     :width: 300pt
