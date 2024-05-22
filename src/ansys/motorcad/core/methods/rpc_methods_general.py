5a3eb0e66a9067ed93428890662742209e57257c
Jack <106958177+JackB-Ansys@users.noreply.github.com>
2024-05-22 11:10:26 +0100

    Update src/ansys/motorcad/core/methods/rpc_methods_general.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- src/ansys/motorcad/core/methods/rpc_methods_general.py
+++ src/ansys/motorcad/core/methods/rpc_methods_general.py
@@ -117,7 +117,7 @@ class _RpcMethodsGeneral:
             Thermal Solution), and ``'Transient'`` (Transient Thermal Solution).
         file_path : str
             Absolute path for the CSV file. The default is the Windows
-            directory on the C: drive. The filepath must include the name
+            directory on the C: drive. The file path must include the name
             of the file, with a CSV extension. Use the ``r'filepath'``
             syntax to force Python to ignore special characters.
         """
