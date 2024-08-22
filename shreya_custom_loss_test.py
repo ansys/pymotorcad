9712f18f4af583cb6aadaacb0bc8799c89e9fa2e
ShreyaGursal <shreya.gursal@ansys.om>
2024-08-22 13:19:41 +0100

    Add custom loss function implemented.

--- shreya_custom_loss_test.py
+++ shreya_custom_loss_test.py
@@ -8,11 +8,13 @@ mc = MotorCAD(open_new_instance=False)
 # mc.do_magnetic_calculation()
 
 # (print(mc.get_array_variable("CustomLoss_name_internal_lab", 0))
-# mc.set_variable("NumCustomLossesInternal_Lab", 1)
-# mc.set_array_variable("CustomLoss_name_internal_lab", 0, "new name")
-# mc.set_array_variable("CustomLoss_Function_Internal_Lab", 0, "Idc")
-# mc.set_array_variable("CustomLoss_Type_Internal_Lab", 0, "Mechanical")
-# mc.set_array_variable("CustomLoss_ThermalNode_Internal_Lab", 0, -1)
+# mc.set_variable("NumCustomLossesInternal_Lab", 5)
+# for i in range(5):
+#     mc.set_array_variable("CustomLoss_name_internal_lab", i, "new name" + str(i))
+#     mc.set_array_variable("CustomLoss_Function_Internal_Lab", i, "Idc")
+#     mc.set_array_variable("CustomLoss_Type_Internal_Lab", i, "Mechanical")
+#     mc.set_array_variable("CustomLoss_ThermalNode_Internal_Lab", i, -1)
+
 
 
 def add_internal_custom_loss(name, function, type, thermal_node):
@@ -30,17 +32,19 @@ def add_internal_custom_loss(name, function, type, thermal_node):
 def remove_internal_custom_loss(index):
     # Find number of losses
     no_losses = mc.get_variable("NumCustomLossesInternal_Lab")
-    if index == no_losses:
-        mc.set_variable("NumCustomLossesInternal_Lab", no_losses - 1)
-    # Decrease array length by 1
+
+    # Collapse variables upwards at location of index
+    for i in range(index+1,no_losses):
+        mc.set_array_variable("CustomLoss_name_internal_lab",i-1,mc.get_array_variable("CustomLoss_name_internal_lab", i))
+        mc.set_array_variable("CustomLoss_Function_Internal_Lab",i-1,mc.get_array_variable("CustomLoss_Function_internal_lab", i))
+        mc.set_array_variable("CustomLoss_Type_Internal_Lab",i-1,mc.get_array_variable("CustomLoss_Type_internal_lab", i))
+        mc.set_array_variable("CustomLoss_ThermalNode_Internal_Lab",i-1,mc.get_array_variable("CustomLoss_ThermalNode_internal_lab", i))
+
+        # Decrease array length by 1
     mc.set_variable("NumCustomLossesInternal_Lab", no_losses - 1)
+
+# Define get index from name function?
+
+def remove_internal_custom_loss_name(name):
     pass
 
-
-# def remove_internal_custom_loss(name):
-#     for i in mc.CustomLoss_name_internal_lab:
-#         if mc.CustomLoss_name_internal_lab[i] == name:
-#
-#         else:
-#             pass
-#     pass
