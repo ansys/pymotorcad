0ecbdc355c11ad700decece94984d06cc59ef3a9
ShreyaGursal <shreya.gursal@ansys.om>
2024-08-28 12:20:00 +0100

    Add external Custom Loss function and docstrings.

--- shreya_custom_loss_test.py
+++ shreya_custom_loss_test.py
@@ -7,7 +7,7 @@ mc = MotorCAD(open_new_instance=False)
 
 # mc.do_magnetic_calculation()
 
-# (print(mc.get_array_variable("CustomLoss_name_internal_lab", 0))
+# print(mc.get_array_variable("CustomLoss_name_internal_lab", 0))
 # mc.set_variable("NumCustomLossesInternal_Lab", 5)
 # for i in range(5):
 #     mc.set_array_variable("CustomLoss_name_internal_lab", i, "new name" + str(i))
@@ -19,40 +19,41 @@ mc = MotorCAD(open_new_instance=False)
 
 def add_internal_custom_loss(name, function, type, thermal_node):
     # Find number of losses
-    no_losses = mc.get_variable("NumCustomLossesInternal_Lab")
+    no_internal_losses = mc.get_variable("NumCustomLossesInternal_Lab")
     # Increase array length by 1
-    mc.set_variable("NumCustomLossesInternal_Lab", no_losses+1)
+    mc.set_variable("NumCustomLossesInternal_Lab", no_internal_losses+1)
     # Define properties
-    mc.set_array_variable("CustomLoss_name_internal_lab", no_losses, name)
-    mc.set_array_variable("CustomLoss_Function_Internal_Lab", no_losses, function)
-    mc.set_array_variable("CustomLoss_Type_Internal_Lab", no_losses, type)
-    mc.set_array_variable("CustomLoss_ThermalNode_Internal_Lab", no_losses, thermal_node)
+    mc.set_array_variable("CustomLoss_name_internal_lab", no_internal_losses, name)
+    mc.set_array_variable("CustomLoss_Function_Internal_Lab", no_internal_losses, function)
+    mc.set_array_variable("CustomLoss_Type_Internal_Lab", no_internal_losses, type)
+    mc.set_array_variable("CustomLoss_ThermalNode_Internal_Lab", no_internal_losses, thermal_node)
 
+def add_external_custom_loss(name, power_function, voltage_function):
+    no_external_losses = mc.get_variable("NumCustomLossesExternal_Lab")
+    mc.set_variable("NumCustomLossesExternal_Lab", no_external_losses + 1)
+    mc.set_array_variable('CustomLoss_Name_External_Lab', no_external_losses, name)
+    mc.set_array_variable('CustomLoss_PowerFunction_External_Lab', no_external_losses, power_function)
+    mc.set_array_variable('CustomLoss_VoltageFunction_External_Lab', no_external_losses, voltage_function)
 
 def remove_internal_custom_loss(index):
     # Find number of losses
-    no_losses = mc.get_variable("NumCustomLossesInternal_Lab")
+    no_internal_losses = mc.get_variable("NumCustomLossesInternal_Lab")
 
     # Collapse variables upwards at location of index
-    for i in range(index+1,no_losses):
+    for i in range(index+1,no_internal_losses):
         mc.set_array_variable("CustomLoss_name_internal_lab",i-1,mc.get_array_variable("CustomLoss_name_internal_lab", i))
         mc.set_array_variable("CustomLoss_Function_Internal_Lab",i-1,mc.get_array_variable("CustomLoss_Function_internal_lab", i))
         mc.set_array_variable("CustomLoss_Type_Internal_Lab",i-1,mc.get_array_variable("CustomLoss_Type_internal_lab", i))
         mc.set_array_variable("CustomLoss_ThermalNode_Internal_Lab",i-1,mc.get_array_variable("CustomLoss_ThermalNode_internal_lab", i))
 
         # Decrease array length by 1
-    mc.set_variable("NumCustomLossesInternal_Lab", no_losses - 1)
-
-# Define get index from name function?
+    mc.set_variable("NumCustomLossesInternal_Lab", no_internal_losses - 1)
 
 def remove_internal_custom_loss_name(name):
-    no_losses = mc.get_variable("NumCustomLossesInternal_Lab")
-    for i in range(no_losses):
+    no_internal_losses = mc.get_variable("NumCustomLossesInternal_Lab")
+    for i in range(no_internal_losses):
         if name == mc.get_array_variable("CustomLoss_name_internal_lab", i):
             remove_internal_custom_loss(i)
             break
 
-add_internal_custom_loss("name","Idc", "Mech", "four")
 
-# Can set Custom Loss Type to something other than ELectrical/Mechanical
-# If thermal node is set incorrectly, returns error
