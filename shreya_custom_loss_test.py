a2aeb6d1d32e08a33501d00ce5813485c47c0b58
ShreyaGursal <shreya.gursal@ansys.om>
2024-09-11 14:14:51 +0100

    Refactored global variables and added error handling from removing custom loss by name.

--- shreya_custom_loss_test.py
+++ shreya_custom_loss_test.py
@@ -4,57 +4,57 @@ from tkinter.font import names
 # from numpy import error_message
 
 from ansys.motorcad.core import MotorCAD
-from examples.links.ece_export_for_twinbuilder import index
+# from examples.links.ece_export_for_twinbuilder import index
 
 # from examples.links.ece_export_for_twinbuilder import index
 
 mc = MotorCAD(open_new_instance=False)
 
 # Global variables
-kNumCustomLossesInternal_Lab = "NumCustomLossesInternal_Lab"
-kCustomLoss_name_internal_lab = "CustomLoss_name_internal_lab"
-kCustomLoss_Function_Internal_Lab = "CustomLoss_Function_Internal_Lab"
-kCustomLoss_Type_Internal_Lab = "CustomLoss_Type_Internal_Lab"
-kCustomLoss_ThermalNode_Internal_Lab = "CustomLoss_ThermalNode_Internal_Lab"
-kNumCustomLossesExternal_Lab = "NumCustomLossesExternal_Lab"
-kCustomLoss_Name_External_Lab = "CustomLoss_Name_External_Lab"
-kCustomLoss_PowerFunction_External_Lab = "CustomLoss_PowerFunction_External_Lab"
-kCustomLoss_VoltageFunction_External_Lab = "CustomLoss_VoltageFunction_External_Lab"
+k_num_custom_losses_internal_lab = "NumCustomLossesInternal_Lab"
+k_custom_loss_name_internal_lab = "CustomLoss_name_internal_lab"
+k_custom_loss_function_internal_lab = "CustomLoss_Function_Internal_Lab"
+k_custom_loss_type_internal_lab = "CustomLoss_Type_Internal_Lab"
+k_custom_loss_thermal_node_internal_lab = "CustomLoss_ThermalNode_Internal_Lab"
+k_num_custom_losses_external_lab = "NumCustomLossesExternal_Lab"
+k_custom_loss_name_external_lab = "CustomLoss_Name_External_Lab"
+k_custom_loss_power_function_external_lab = "CustomLoss_PowerFunction_External_Lab"
+k_custom_loss_voltage_function_external_lab = "CustomLoss_VoltageFunction_External_Lab"
 
 def add_internal_custom_loss(name, function, type, thermal_node):
     type = type.capitalize()
     # Internal Custom Loss Type is case-sensitive in MotorCAD. Added a line to match the required format.
     if type not in ["Electrical", "Mechanical"]:
         raise ValueError("Thermal Loss Type must be Electrical or Mechanical")
-    if mc.get_node_exists(thermal_node) == False:
+    if not mc.get_node_exists(thermal_node):
         raise ValueError("Thermal node does not exist")
     else:
         # Find number of losses
-        no_internal_losses = mc.get_variable(kNumCustomLossesInternal_Lab)
+        no_internal_losses = mc.get_variable(k_num_custom_losses_internal_lab)
         # Increase array length by 1
-        mc.set_variable(kNumCustomLossesInternal_Lab, no_internal_losses + 1)
+        mc.set_variable(k_num_custom_losses_internal_lab, no_internal_losses + 1)
         # Define properties
-        mc.set_array_variable(kCustomLoss_name_internal_lab, no_internal_losses, name)
-        mc.set_array_variable(kCustomLoss_Function_Internal_Lab, no_internal_losses, function)
-        mc.set_array_variable(kCustomLoss_Type_Internal_Lab, no_internal_losses, type)
-        mc.set_array_variable(kCustomLoss_ThermalNode_Internal_Lab, no_internal_losses, thermal_node)
+        mc.set_array_variable(k_custom_loss_name_internal_lab, no_internal_losses, name)
+        mc.set_array_variable(k_custom_loss_function_internal_lab, no_internal_losses, function)
+        mc.set_array_variable(k_custom_loss_type_internal_lab, no_internal_losses, type)
+        mc.set_array_variable(k_custom_loss_thermal_node_internal_lab, no_internal_losses, thermal_node)
 
 def add_external_custom_loss(name, power_function, voltage_function):
-    no_external_losses = mc.get_variable(kNumCustomLossesExternal_Lab)
-    mc.set_variable(kNumCustomLossesExternal_Lab, no_external_losses + 1)
-    mc.set_array_variable(kCustomLoss_Name_External_Lab, no_external_losses, name)
-    mc.set_array_variable(kCustomLoss_PowerFunction_External_Lab, no_external_losses, power_function)
-    mc.set_array_variable(kCustomLoss_VoltageFunction_External_Lab, no_external_losses, voltage_function)
+    no_external_losses = mc.get_variable(k_num_custom_losses_external_lab)
+    mc.set_variable(k_num_custom_losses_external_lab, no_external_losses + 1)
+    mc.set_array_variable(k_custom_loss_name_external_lab, no_external_losses, name)
+    mc.set_array_variable(k_custom_loss_power_function_external_lab, no_external_losses, power_function)
+    mc.set_array_variable(k_custom_loss_voltage_function_external_lab, no_external_losses, voltage_function)
 
 def remove_internal_custom_loss(name):
-    index = _get_index_from_name(name, kCustomLoss_name_internal_lab)
-    _motorcad_array_pop(index, kNumCustomLossesInternal_Lab, [kCustomLoss_name_internal_lab, kCustomLoss_Function_Internal_Lab,
-                                                                                kCustomLoss_Type_Internal_Lab, kCustomLoss_ThermalNode_Internal_Lab])
+    index = _get_index_from_name(name, k_num_custom_losses_internal_lab,  k_custom_loss_name_internal_lab)
+    _motorcad_array_pop(index, k_num_custom_losses_internal_lab, [k_custom_loss_name_internal_lab, k_custom_loss_function_internal_lab,
+                                                                  k_custom_loss_type_internal_lab, k_custom_loss_thermal_node_internal_lab])
 
 def remove_external_custom_loss(name):
-    index = _get_index_from_name(name, kCustomLoss_Name_External_Lab)
-    _motorcad_array_pop(index, kNumCustomLossesExternal_Lab,[kCustomLoss_Name_External_Lab, kCustomLoss_PowerFunction_External_Lab,
-                                                                                 kCustomLoss_VoltageFunction_External_Lab])
+    index = _get_index_from_name(name,k_num_custom_losses_external_lab, k_custom_loss_name_external_lab)
+    _motorcad_array_pop(index, k_num_custom_losses_external_lab, [k_custom_loss_name_external_lab, k_custom_loss_power_function_external_lab,
+                                                                  k_custom_loss_voltage_function_external_lab])
 
 def _motorcad_array_pop(index, var_length_array, list_of_var_names):
     array_length = mc.get_variable(var_length_array)
@@ -66,14 +66,15 @@ def _motorcad_array_pop(index, var_length_array, list_of_var_names):
 
     mc.set_variable(var_length_array, array_length - 1)
 
-def _get_index_from_name(name, variable):
-    for i in range(len(variable)):
-        if name == mc.get_array_variable(variable, i):
+def _get_index_from_name(name, var_length_array, variable_name):
+    array_length = mc.get_variable(var_length_array)
+    for i in range(array_length):
+        if name == mc.get_array_variable(variable_name, i):
             index = i
-            return index
-
-
-
+            break
+    else:
+        raise NameError("Provided name is not listed")
+    return index
 
 
 
