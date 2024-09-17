877b5c25a354305ddca43bead8e4f702851b0b49
ShreyaGursal <shreya.gursal@ansys.om>
2024-09-17 08:59:50 +0100

    Modified tests for adding and removing external/internal custom loss.

--- tests/test_lab.py
+++ tests/test_lab.py
@@ -1,5 +1,5 @@
 from RPC_Test_Common import reset_to_default_file
-
+import pytest
 
 def test_model_build_lab(mc):
     reset_to_default_file(mc)
@@ -48,14 +48,22 @@ def test_calculate_thermal_lab(mc):
     mc.calculate_thermal_lab()
     # Not sure how to test this has run successfully other than checking for exception
 
-def test_add_internal_custom_loss(mc):
+def test_internal_custom_loss_functions(mc):
+    no_internal_losses = mc.get_variable("NumCustomLossesInternal_Lab")
     name = "Stator iron Loss Connection"
     power_loss_function = "Speed**2 * (5.4E-9 * Iron_Loss_Stator - 1.7E-5 * Iron_Loss_Stator**2 + 1.43)"
     type = "Electrical"
+    incorrect_type = "Type"
     thermal_node = 2
-    no_internal_losses = mc.get_variable("NumCustomLossesInternal_Lab")
+    incorrect_thermal_node = -2
+    removed_name = "Bearing Connection"
+    removed_power_loss_function = "1E-2 * Mechanical_Loss **2) * Speed / 10000 + min(5, Speed)"
+    removed_type = "Mechanical"
+    removed_thermal_node = 4
 
-    mc.add_internal_custom_loss(name,power_loss_function,type,thermal_node)
+    mc.add_internal_custom_loss(name, power_loss_function, type, thermal_node)
+    mc.add_internal_custom_loss(removed_name, removed_power_loss_function, removed_type, removed_thermal_node)
+    mc.remove_internal_custom_loss(removed_name)
 
     assert mc.get_variable("NumCustomLossesInternal_Lab") == no_internal_losses + 1
     assert mc.get_array_variable("CustomLoss_name_internal_lab", no_internal_losses) == name
@@ -63,23 +71,28 @@ def test_add_internal_custom_loss(mc):
     assert mc.get_array_variable("CustomLoss_Type_Internal_Lab", no_internal_losses) == type
     assert mc.get_array_variable("CustomLoss_ThermalNode_Internal_Lab", no_internal_losses) == thermal_node
 
+    with pytest.raises(ValueError):
+        mc.add_internal_custom_loss(name,power_loss_function,incorrect_type,thermal_node)
 
-def test_add_external_custom_loss(mc):
+    with pytest.raises(ValueError):
+        mc.add_internal_custom_loss(name,power_loss_function,type, incorrect_thermal_node)
+
+def test_external_custom_loss_functions(mc):
+    no_external_losses = mc.get_variable("NumCustomLossesExternal_Lab")
     name = "Battery Loss"
     power_loss_function = "(1E-2 * Mechanical_Loss**2) * Speed/10000 + min(5, Speed)"
     voltage_drop_function = "Idc * 2.5E-2"
-    no_external_losses = mc.get_variable("NumCustomLossesExternal_Lab")
+    removed_name = "DC Harness Loss"
+    removed_power_loss_function = "Idc**2 * 1E-2"
+    removed_voltage_drop_function = "Idc * 2.5E-2"
 
+    mc.add_external_custom_loss(removed_name, removed_power_loss_function, removed_voltage_drop_function)
     mc.add_external_custom_loss(name, power_loss_function, voltage_drop_function)
+    mc.remove_external_custom_loss(removed_name)
 
     assert mc.get_variable("NumCustomLossesExternal_Lab") == no_external_losses + 1
     assert mc.get_array_variable("CustomLoss_Name_External_Lab", no_external_losses) == name
     assert mc.get_array_variable("CustomLoss_PowerFunction_External_Lab", no_external_losses) == power_loss_function
     assert mc.get_array_variable("CustomLoss_VoltageFunction_External_Lab", no_external_losses) == voltage_drop_function
 
-def test_remove_internal_custom_loss(mc):
-    pass
-
-def test_remove_external_custom_loss(mc):
-    pass
 
