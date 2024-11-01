0cf8a75e19159fec316adf3cd1f95165a28826ee
Matthew Jones <107551269+matthewAnsys@users.noreply.github.com>
2024-11-01 16:59:48 +0000

    Add new region duplication settings API (#385)
    * Initial commit
    
    * Update unit tests
    
    * Update linked region to update both regions in single call
    
    * fix test
    
    * Improve testing coverage
    
    * Update string ""
    
    * Improve test coverage for linked_region getter
    
    ---------
    
    Co-authored-by: Jack Davies <jack.davies@ansys.com>
    Co-authored-by: Jack Davies <80680946+jgsdavies@users.noreply.github.com>
    (cherry picked from commit 91a49dd2d08055ae1a9f78caa38d811252457f1f)

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -353,6 +353,7 @@ def test_region_from_json():
         "child_names": ["Duct", "Duct_1"],
         "region type": "Adaptive Region",
         "mesh_length": 0.035,
+        "singular": False,
     }
 
     test_region = geometry.Region()
@@ -366,7 +367,8 @@ def test_region_from_json():
     test_region.entities = []
     test_region.parent_name = "Insulation"
     test_region._child_names = ["Duct", "Duct_1"]
-    test_region.mesh_length = 0.035
+    test_region.mesh_length = (0.035,)
+    test_region.singular = (False,)
 
     region = geometry.Region._from_json(raw_region)
 
@@ -386,6 +388,8 @@ def test_region_to_json():
         "parent_name": "Insulation",
         "region_type": "Adaptive Region",
         "mesh_length": 0.035,
+        "singular": True,
+        "on_boundary": False,
     }
 
     test_region = geometry.Region()
@@ -399,6 +403,7 @@ def test_region_to_json():
     test_region.entities = []
     test_region.parent_name = "Insulation"
     test_region.mesh_length = 0.035
+    test_region.singular = True
 
     assert test_region._to_json() == raw_region
 
@@ -407,6 +412,26 @@ def test_region_is_closed():
     region = generate_constant_region()
 
     assert region.is_closed()
+
+
+def test_set_linked_region():
+    region = generate_constant_region()
+
+    region_linked = Region()
+    region_linked.name = "linked_region_test"
+    # set linked region
+    region.linked_region = region_linked
+
+    assert region._linked_region.name == region_linked.name
+    assert region_linked.linked_region.name == region.name
+
+
+def test_set_singular_region():
+    region = generate_constant_region()
+    region.singular = True
+
+    assert region._singular is True
+    assert region.singular is True
 
 
 def test_region_contains_same_entities():
