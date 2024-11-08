31231c516e9b35bcc77bd2e8679fc1e214520567
James Packer <106395938+james-packer@users.noreply.github.com>
2024-11-08 22:19:04 +0000

    Fix graph tests following change to motor test file in #411. (#427)
    * Fix graph tests following change to motor test file in #411.
    
    * Use graph point functions in graph tests where possible.
    
    * Remove hardcoding of values from test
    
    (cherry picked from commit 7013e49d0a7704b98f5f56b5fbb4dad038798e92)

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -111,12 +111,15 @@ def test_get_heatflow_graph(mc):
     mc.set_variable("TransientCalculationType", 0)
     mc.do_transient_analysis()
 
-    x, y = mc.get_heatflow_graph("Housing OH [Rear] to Ambient")
+    x, y = mc.get_heatflow_graph("TVent Airgap to Rotor Surface")
+    # This should be the same grap series
     x1, y1 = mc.get_heatflow_graph(400524)
 
-    assert len(x) == len(y) == 9
+    assert len(x) == len(y) == 11
     assert len(x1) == len(y1) == 11
-    assert almost_equal(y1[3], 11.68)
+    assert almost_equal(y1[3], y[3])
+    # Expect heat flow to increase with time for this model
+    assert y[4] > y[3]
 
 
 def test_get_fea_graph(mc):
@@ -127,10 +130,12 @@ def test_get_fea_graph(mc):
 
     mc.do_magnetic_calculation()
 
-    x, y = mc.get_fea_graph("Bt Gap (on load)", 1)
-    assert len(x) == len(y) == 181
-    assert almost_equal(x[16], 4)
-    assert almost_equal(y[16], 0.344792719, 2)
+    x, y = mc.get_fea_graph_point("Bt Gap (on load)", 1, 16, 0)
+    x1, y1 = mc.get_fea_graph("Bt Gap (on load)", 1)
+
+    assert len(x1) == len(y1) == 181
+    assert almost_equal(x1[16], x)
+    assert almost_equal(y1[16], y)
 
 
 def test_get_fea_graph_transient(mc):
@@ -141,10 +146,11 @@ def test_get_fea_graph_transient(mc):
 
     mc.do_magnetic_calculation()
 
-    x, y = mc.get_fea_graph("Br Gap (stator) (on load transient)", 1, 0)
-    assert len(x) == len(y) == 4
-    assert almost_equal(x[1], 120)
-    assert almost_equal(y[1], 0.626116165, 2)
+    x, y = mc.get_fea_graph_point("Br Gap (stator) (on load transient)", 1, 0, 1)
+    x1, y1 = mc.get_fea_graph("Br Gap (stator) (on load transient)", 1, 0)
+    assert len(x1) == len(y1) == 4
+    assert almost_equal(x1[1], x)
+    assert almost_equal(y1[1], y)
 
 
 def test_get_magnetic_3d_graph(mc):
@@ -155,11 +161,12 @@ def test_get_magnetic_3d_graph(mc):
 
     mc.do_magnetic_calculation()
 
+    x, y = mc.get_magnetic_3d_graph_point("Ft_Stator_OL", 1, 1, 1)
     graph_result = mc.get_magnetic_3d_graph("Ft_Stator_OL", 1)
     assert len(graph_result.x) == 481
     assert len(graph_result.y) == 4
     # Note that graph viewer shows in kN, result is in N
-    assert almost_equal(graph_result.data[1][1], -0.560483068, 3)
+    assert almost_equal(graph_result.data[1][1], y)
 
 
 #   #Not fully ready submitted an issue
