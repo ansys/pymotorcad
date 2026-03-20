6369bf3a477e64faf377ece1ea8b0f06a25ebfb4
James Packer <james.packer@ansys.com>
2026-03-20 11:38:52 +0000

    Ensure transient thermal test can run with different Motor-CAD versions.

--- examples/internal_scripting/setup_scripts/setup_script.py
+++ examples/internal_scripting/setup_scripts/setup_script.py
@@ -189,6 +189,9 @@ def run_thermal_transient_demo(mc):
     mc.set_variable("Simple_Transient_Definition", 1)
     mc.set_variable("Simple_Transient_Torque", 100)
     # %%
+    # Set endpoint to fixed duration
+    mc.set_variable("EndPoint_Definition", 0)
+    # %%
     # Solve thermal model
     mc.do_transient_analysis()
 
