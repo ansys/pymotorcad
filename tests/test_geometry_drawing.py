331fab45edfb8bb6f872fde0ee6f7ef280312c51
matthewAnsys <matthew.jones@ansys.com>
2026-05-05 10:17:24 +0100

    Update code style

--- tests/test_geometry_drawing.py
+++ tests/test_geometry_drawing.py
@@ -251,7 +251,7 @@ def test_draw_list_of_coordinates_and_regions_and_entities(mc):
         to_draw.extend(gt["Shaft"].entities)
         to_draw.append(gt["Rotor"])
         to_draw.append(gt["RotorDuctFluidRegion"])
-        #to_draw.append(gt["RotorDuctFluidRegion_1"])
+        # to_draw.append(gt["RotorDuctFluidRegion_1"])
         draw_objects(to_draw, save=path)
 
 
