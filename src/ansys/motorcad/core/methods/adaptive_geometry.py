16c02c8f0aa79b40cbf99e3217c3654df51bceec
James Schaffer <jamessch@synopsys.com>
2026-08-04 10:59:54 +0100

    removed

--- src/ansys/motorcad/core/methods/adaptive_geometry.py
+++ src/ansys/motorcad/core/methods/adaptive_geometry.py
@@ -389,7 +389,6 @@ class _RpcMethodsAdaptiveGeometry:
         material=None,
         colour=None,
         mesh_length=None,
-        lamination_type=None,
         br_multiplier=None,
         magnet_angle=None,
         magnet_polarity=None,
@@ -407,8 +406,6 @@ class _RpcMethodsAdaptiveGeometry:
             RGB colour tuple.
         mesh_length : float, optional
             Mesh length.
-        lamination_type : str, optional
-            Lamination type.
         br_multiplier : float, optional
             Br multiplier applied to the magnet.
         magnet_angle : float, optional
@@ -426,7 +423,6 @@ class _RpcMethodsAdaptiveGeometry:
                 for key, value in {
                     "material": material,
                     "mesh_length": mesh_length,
-                    "lamination_type": lamination_type,
                     "magnet_magfactor": br_multiplier,
                     "magnet_angle": magnet_angle,
                     "magnet_polarity": magnet_polarity,
