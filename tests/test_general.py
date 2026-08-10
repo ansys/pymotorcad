1a0313bd73f317df53e9698ccfc74520295dd3a3
James Schaffer <jamessch@synopsys.com>
2026-08-10 14:23:33 +0100

    added tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -33,6 +33,7 @@ from RPC_Test_Common import (
     reset_to_default_file,
 )
 from ansys.motorcad.core import MotorCAD, MotorCADError
+from ansys.motorcad.core.enums import MotorCADPopupDisplayLevel
 
 
 def kh_to_ms(kh):
@@ -93,6 +94,35 @@ def test_export_results(mc):
     mc.export_results("SteadyState", file_path)
 
     assert os.path.exists(file_path)
+
+
+def test_message_config(mc):
+    if not mc.connection.check_if_feature_exists("motor_cad_messager"):
+        pytest.skip("Motor-CAD Messager is not enabled, skipping test_message_config")
+
+    mc.disable_popups()
+    assert mc.get_popups_enabled() is False
+    mc.enable_popups()
+    assert mc.get_popups_enabled() is True
+
+    mc.set_popup_display_level(MotorCADPopupDisplayLevel.info)
+    assert mc.get_popup_display_level() == MotorCADPopupDisplayLevel.info
+    mc.set_popup_display_level(MotorCADPopupDisplayLevel.error)
+    assert mc.get_popup_display_level() == MotorCADPopupDisplayLevel.error
+    mc.set_popup_display_level(MotorCADPopupDisplayLevel.warning)
+    assert mc.get_popup_display_level() == MotorCADPopupDisplayLevel.warning
+    mc.set_popup_display_level(MotorCADPopupDisplayLevel.fatal)
+    assert mc.get_popup_display_level() == MotorCADPopupDisplayLevel.fatal
+
+
+def test_verbose_message_config(mc):
+    if not mc.connection.check_if_feature_exists("motor_cad_messager"):
+        pytest.skip("Motor-CAD Messager is not enabled, skipping test_verbose_message_config")
+
+    mc.disable_verbose_messages()
+    assert mc.get_verbose_messages_enabled() is False
+    mc.enable_verbose_messages()
+    assert mc.get_verbose_messages_enabled() is True
 
 
 def test_load_dxf_file():
