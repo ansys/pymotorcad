48d54e2c2eb21209a7c7f748401f3bbbbf201e1f
ShreyaGursal <sgursal@synopsys.com>
2026-08-20 14:20:29 +0100

    Conftest.py change

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def pytest_sessionstart(session):
 @pytest.fixture(scope="session")
 def mc():
     """Set up test environment for whole unit of tests"""
-    motorcad_instance = MotorCAD(use_original_licensing=False)
+    motorcad_instance = MotorCAD(use_new_license_type=True)
     # Disable messages if opened with UI
     motorcad_instance.set_variable("MessageDisplayState", 2)
     reset_to_default_file(motorcad_instance)
