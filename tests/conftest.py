927976a671b52bef96b86e902c50442b768ad789
Jack Davies <jdavies@synopsys.com>
2026-09-14 12:24:38 +0100

    correct case

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import warnings
-
 import pytest
 
 from RPC_Test_Common import reset_temp_file_folder, reset_to_default_file
@@ -73,9 +71,8 @@ def mc_fea_old():
 @pytest.fixture()
 def mc_headless():
     """Launch a fully headless Motor-CAD instance for testing GUI-only method warnings."""
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", UserWarning)
-        instance = MotorCAD(full_headless_beta=True)
+    mc_headless = MotorCAD(full_headless_beta=True)
+    reset_to_default_file(mc_headless)
 
-    yield instance
-    instance.quit()
+    yield mc_headless
+    mc_headless.quit()
